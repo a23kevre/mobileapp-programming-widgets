047dc5dacb2cf4539e3c023f6b45d6a9c55e468c
a23kevre <a23kevre@student.his.se>
2024-04-08 15:50:15 +0200

    lagt till en edittext som har en placeholder.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.EditText; // <--- this is the import that is required for EditText to work in your code
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
